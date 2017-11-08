a7f525e8ffabc0393cb47bc58f19800d69512c6d
Rob Yurkowski <rob@yurkowski.net>
2017-11-08 00:23:16 -0500

    Don't chain tasks.

--- base/tasks/dev.rb
+++ base/tasks/dev.rb
@@ -2,12 +2,12 @@ namespace :dev do
   namespace :composer do
     desc "Runs composer install on the VM."
     task :install do
-      run "vagrant ssh -- -t 'cd #{REMOTE_APP_FOLDER} && composer install'"
+      run "vagrant ssh -- -t 'cd #{REMOTE_APP_FOLDER}; composer install'"
     end
 
     desc "Runs composer update on the VM."
     task :update do
-      run "vagrant ssh -- -t 'cd #{REMOTE_APP_FOLDER} && composer update'"
+      run "vagrant ssh -- -t 'cd #{REMOTE_APP_FOLDER}; composer update'"
     end
   end
 end
