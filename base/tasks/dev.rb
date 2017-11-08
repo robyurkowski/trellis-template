089acb3b9d2f31a18121ecae75bf9d5b19ce1a4e
Rob Yurkowski <rob@yurkowski.net>
2017-11-08 00:20:48 -0500

    Fix composer tasks.

--- base/tasks/dev.rb
+++ base/tasks/dev.rb
@@ -1,11 +1,11 @@
 namespace :dev do
   namespace :composer do
     task :install do
-      run "vagrant ssh -c 'cd #{REMOTE_APP_FOLDER} && composer install'"
+      run "vagrant ssh -- -t 'cd #{REMOTE_APP_FOLDER} && composer install'"
     end
 
     task :update do
-      run "vagrant ssh -c 'cd #{REMOTE_APP_FOLDER} && composer update'"
+      run "vagrant ssh -- -t 'cd #{REMOTE_APP_FOLDER} && composer update'"
     end
   end
 end
