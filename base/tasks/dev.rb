a20f1d2c8b0a78975f080554ea705da7fe83d184
Rob Yurkowski <rob@yurkowski.net>
2017-11-08 00:21:24 -0500

    Add desc for tasks.

--- base/tasks/dev.rb
+++ base/tasks/dev.rb
@@ -1,9 +1,11 @@
 namespace :dev do
   namespace :composer do
+    desc "Runs composer install on the VM."
     task :install do
       run "vagrant ssh -- -t 'cd #{REMOTE_APP_FOLDER} && composer install'"
     end
 
+    desc "Runs composer update on the VM."
     task :update do
       run "vagrant ssh -- -t 'cd #{REMOTE_APP_FOLDER} && composer update'"
     end
